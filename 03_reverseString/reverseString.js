3d1ee89db129cd39aea503ffd826a8eedf3e26fe
alc-yk <125387583+alc-yk@users.noreply.github.com>
2023-08-25 14:46:39 -0500

    Finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    var lastElement = '';
+    var reverseWord = '';
+    word.split("");
+    let x = 1
+
+    for(i=0; i<word.length; i++){
+        lastElement = word[word.length -x];
+        x++;
+        reverseWord += lastElement; 
+    }
+    
+    return reverseWord;
 };
 
 // Do not edit below this line
