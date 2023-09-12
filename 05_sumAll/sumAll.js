f57a850c3186842c83c04d0d08ca296b8de49702
alc-yk <125387583+alc-yk@users.noreply.github.com>
2023-09-12 12:14:43 -0500

    FInished java exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let finalValue = 0;
+
+    if (typeof int2 != "number" || typeof int1 != "number") {return "ERROR";}
+    if (int1 < 0 || int2 < 0) {return "ERROR";}
+    
+    if (int1 < int2 ) {
+        for(i = int1; i<=int2; i++)
+            {
+                finalValue = finalValue + i;
+            }
+    }
+    else if (int1 > int2 ) {
+        for(i = int2; i<=int1; i++)
+            {
+                finalValue = finalValue + i;
+            }
+        }
+
+    return finalValue;
 
 };
 
